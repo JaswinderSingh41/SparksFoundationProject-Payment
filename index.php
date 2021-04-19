866aafa0b7a2f3fa8a4adccaf4ad3aec27d5d7d0
Jaswinder Singh <74896683+JaswinderSingh41@users.noreply.github.com>
2021-04-19 17:02:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php session_start();
+$_SESSION['page']="home";
+?>
+<html>
+    <head>
+        <title>Help me in getting Funds!</title>
+        <link rel="stylesheet" href="style.css">
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+    </head>
+    <body style="background-color: black">
+        <header>
+            <?php include './Header.php'; ?>
+<br>
+<div style="font-family: 'Montserrat',sans-serif;" class="container">
+                    <h1>SPARKS FOUNDATION PROJECT </h1><br>
+                    <p><h3>"Payment Gateway"<br>The Sparks Foundation aims to inspire students, help them innovate and let them integrate to build the next generation humankind. <br></h3></p>
+                    <br><h3 style="text-indent:15em;";> BY Jaswinder Singh</h3>
+                </div>
+              </div>
+        </header>
+    </body>
+</html>
